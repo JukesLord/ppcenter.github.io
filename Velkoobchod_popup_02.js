141e77f9348e9e0f658fa3a2a87a1b1d44e1c329
JukesLord <111180246+JukesLord@users.noreply.github.com>
2023-05-02 09:42:04 +0200

    Create Velkoobchod_popup_02.js

---
+++ Velkoobchod_popup_02.js
@@ -0,0 +1,10 @@
+if (zakaznikId2 == 18){
+    $("body").prepend('<div class="velkoobchod-popup"><p>Pro velkoobchodní odběratele jsme připravili nové rozhraní. Na tomto e-shopu už uvidíte pouze maloobchodní ceny.</p><p>Pro nákup se přihlaste na <a href="https://b2b.ppcentershop.cz/prihlaseni/">b2b.ppcentershop.cz.</a></p><button class="prejitNaVelkoobchod">Přejít na B2B</button><button class="zavritPopup">Zavřít</button><p>*Při prvním přihlášení je z bezpečnostních důvodů zapotřebí vytvořit nové heslo. Stránka vás k tomuto kroku automaticky navede.</p></div>');
+    $(".velkoobchod-popup").show();
+    $(".zavritPopup").on("click", function() {
+      $(".velkoobchod-popup").hide();
+      });
+     $(".prejitNaVelkoobchod").click(function(){
+      document.location.href="https://b2b.ppcentershop.cz/prihlaseni/";
+    });
+}
