78e9b541846a2620830adfed78b9f4bd71445ddc
FilipHosek <111180246+JukesLord@users.noreply.github.com>
2024-03-12 15:28:08 +0100

    Update Velkoobchod_ppcentershop_01.js

--- Velkoobchod_ppcentershop_01.js
+++ Velkoobchod_ppcentershop_01.js
@@ -4,3 +4,25 @@ $(function () {
     $(this).parent().toggleClass("active");
   });
 });
+
+if (document.body.classList.contains("type-detail")) {
+document.addEventListener("DOMContentLoaded", function (event) {
+		function cardHeight() {
+			if ($(".card-text").length > 1) {
+				let cardHeight = 0;
+
+				$(".card-text").css("min-height", "");
+
+				$(".card-text").each(function () {
+					var thisHeight = $(this).outerHeight();
+					if (thisHeight > cardHeight) {
+						cardHeight = thisHeight;
+					}
+				});
+				$(".card-text").css("min-height", cardHeight);
+			}
+		}
+		cardHeight();
+		window.addEventListener("resize", cardHeight);
+});
+}
